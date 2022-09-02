78769c97ec87699f5dda26ffa858dfb4023fb742
CalebeSG <calebeguimaraes10@gmail.com>
2022-09-02 08:50:20 -0300

    created products routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import products from './data/products'
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('API running...')
+})
+
+app.get('/api/product', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/product/:id', (req, res) => {
+  const id = req.params.id
+  const product = products.find(item => item._id === id)
+  res.json(product)
 })
 
 app.listen(3333, () => console.log('SERVER RUNNING: POST 3333'))
