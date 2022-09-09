4023c515945b73ce43598f361565f3a7dff7d7cc
CalebeSG <calebeguimaraes10@gmail.com>
2022-09-09 09:02:00 -0300

    fixed

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -1,18 +1,19 @@
 import { Request, Response } from 'express'
+import asyncHandler from 'express-async-handler'
 import Product from '../models/Product'
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
-const getProducts = async (req: Request, res: Response) => {
+const getProducts = asyncHandler(async (req: Request, res: Response) => {
   const products = await Product.find({})
   res.json(products)
-}
+})
 
 // @desc    Fetch single product
 // @route   GET /api/products/:id
 // @access  Public
-const getProductById = async (req: Request, res: Response) => {
+const getProductById = asyncHandler(async (req: Request, res: Response) => {
   const id = req.params.id
   const product = await Product.findById(id)
 
@@ -22,6 +23,6 @@ const getProductById = async (req: Request, res: Response) => {
   }
 
   res.json(product)
-}
+})
 
 export { getProducts, getProductById }
