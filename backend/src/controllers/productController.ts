2990cf79181d10b87e3bbf849c644a6627a3b6ec
CalebeSG <calebeguimaraes10@gmail.com>
2022-10-21 12:42:47 -0300

    create product resource

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -42,4 +42,67 @@ const deleteProduct = asyncHandler(async (req: Request, res: Response) => {
   res.status(204).send()
 })
 
-export { getProducts, getProductById, deleteProduct }
+// @desc    Create product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req: Request, res: Response) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.body.user._id,
+    image: '/image/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createdProduct = await product.save()
+
+  res.status(201).json(createdProduct)
+})
+
+// @desc    Update product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req: Request, res: Response) => {
+  const {
+    name,
+    price,
+    image,
+    brand,
+    category,
+    countInStock,
+    numReviews,
+    description,
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (!product) {
+    res.status(404)
+    throw new Error('Product not found!')
+  }
+
+  product.name = name
+  product.price = price
+  product.image = image
+  product.band = brand
+  product.category = category
+  product.countInStock = countInStock
+  product.numReviews = numReviews
+  product.description = description
+
+  const updatedProduct = await product.save()
+
+  res.json(updatedProduct)
+})
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+}
--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -5,11 +5,15 @@ import {
   getProductById,
   getProducts,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController'
 import { protect, admin } from '../middleware/authMiddleware'
 
 routes.get('/', getProducts)
+routes.post('/', protect, admin, createProduct)
 routes.get('/:id', getProductById)
 routes.delete('/:id', protect, admin, deleteProduct)
+routes.put('/:id', protect, admin, updateProduct)
 
 export default routes
