d22265cbf939e9d776049baf672405715c8dc7e4
CalebeSG <calebeguimaraes10@gmail.com>
2022-10-25 08:59:32 -0300

    config upload image

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv'
+import path from 'path'
 import express from 'express'
 import cors from 'cors'
 
@@ -6,6 +7,7 @@ import connectDB from './config/db'
 import productRoutes from './routes/productRoutes'
 import userRoutes from './routes/userRoutes'
 import orderRoutes from './routes/orderRoutes'
+import uploadRoutes from './routes/uploadRoutes'
 import { errorHandler, notFound } from './middleware/errorMiddleware'
 
 dotenv.config() //uPDATE
@@ -20,10 +22,14 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
