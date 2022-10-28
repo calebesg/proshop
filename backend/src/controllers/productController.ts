ad7f558ae8c8070fabc73c5444718ba28c68e868
CalebeSG <calebeguimaraes10@gmail.com>
2022-10-28 08:40:12 -0300

    created product review endpoint

--- backend/src/controllers/productController.ts
+++ backend/src/controllers/productController.ts
@@ -99,10 +99,55 @@ const updateProduct = asyncHandler(async (req: Request, res: Response) => {
   res.json(updatedProduct)
 })
 
+// @desc    Create product review
+// @route   POST /api/products/:id/review
+// @access  Private/Admin
+const createProductReview = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { rating, comment } = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if (!product) {
+      res.status(404)
+      throw new Error('Product not found!')
+    }
+
+    const alreadyReviewed = product.reviews.find(
+      r => r.user?.toString() === req.body.user._id.toString()
+    )
+
+    if (alreadyReviewed) {
+      res.status(400)
+      throw new Error('Product already reviewed!')
+    }
+
+    const review = {
+      name: req.body.user.name,
+      rating: +rating,
+      comment,
+      user: req.body.user._id,
+    }
+
+    product.reviews.push(review)
+
+    product.numReviews = product.reviews.length
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length
+
+    await product.save()
+
+    res.status(201).send()
+  }
+)
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 }
--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -16,6 +16,12 @@ const reviewSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: 'User',
+    },
   },
   { timestamps: true }
 )
